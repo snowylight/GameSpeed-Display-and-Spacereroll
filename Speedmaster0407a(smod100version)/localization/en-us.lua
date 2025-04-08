7173417b53d3c0173abbbd5f6f73f59dabfbe19b
sishui <110089805+snowylight@users.noreply.github.com>
2025-04-08 10:24:01 +0800

    Add files via upload

---
+++ Speedmaster0407a(smod100version)/localization/en-us.lua
@@ -0,0 +1,9 @@
+return {
+    misc={
+        dictionary = {
+            k_speedmaster = "Current Speed X",
+            k_speedmastersave = "Save Run",
+            k_speedmasterload = "Load Run",
+        },
+    },
+}
---
+++ Speedmaster0407a(smod100version)/localization/zh_CN.lua
@@ -0,0 +1,9 @@
+return {
+    misc={
+        dictionary = {
+            k_speedmaster = "当前倍速X",
+            k_speedmastersave = "局内存档",
+            k_speedmasterload = "局内读档",
+        },
+    },
+}
