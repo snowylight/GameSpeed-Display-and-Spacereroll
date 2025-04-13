871d78a7ad075e6440983f832f9d0f39fe9d5193
sishui <110089805+snowylight@users.noreply.github.com>
2025-04-13 21:11:50 +0800

    Delete Speedmaster0407a(smod100version) directory

--- Speedmaster0407a(smod100version)/localization/en-us.lua
+++
@@ -1,9 +0,0 @@
-return {
-    misc={
-        dictionary = {
-            k_speedmaster = "Current Speed X",
-            k_speedmastersave = "Save Run",
-            k_speedmasterload = "Load Run",
-        },
-    },
-}
--- Speedmaster0407a(smod100version)/localization/zh_CN.lua
+++
@@ -1,9 +0,0 @@
-return {
-    misc={
-        dictionary = {
-            k_speedmaster = "当前倍速X",
-            k_speedmastersave = "局内存档",
-            k_speedmasterload = "局内读档",
-        },
-    },
-}
